0986cad4cbd84391ac6ce52e72b03e2d5137b5b1
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2023-08-02 00:27:49 +0300

    Logo and fixes

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,21 +1,36 @@
 import React,{useState, useEffect, useRef} from "react";
-import {StyleSheet,TextInput, View} from 'react-native'
+import {StyleSheet,TextInput, View,Image} from 'react-native'
 import { useTheme } from '@react-navigation/native';
 import {Text} from "react-native";
+
+
 
 
 const Home = () => {
   const colors = useTheme().colors;
     
+
+  const styles = StyleSheet.create({
+    container: {
+      width: 450, 
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center'
+    },
+  });
   
     return (
       <>
       
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ color: colors.text }}>
-        Beatiful Home Screen
-      </Text>
+      <View style={styles.container}>
+        <Image source={require('./img/Fingrid_logo_RGB.png')}
+                style={{width: 375, height: 70}} />
+      
       </View>
+      <View style={styles.container}>
+        <Text>This is project was made as an hobby during summer 2023</Text>
+      </View>
+
       
       </>
       );
